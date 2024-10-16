e446effb060135a19f7834f13a5db9ecea7fedeb
renzoDC15 <renzo.carianga@gmail.com>
2024-10-16 10:26:03 +0800

    replace context menu on listleads

--- app/Filament/Resources/LeadResource/Pages/ListLeads.php
+++ app/Filament/Resources/LeadResource/Pages/ListLeads.php
@@ -3,13 +3,33 @@
 namespace App\Filament\Resources\LeadResource\Pages;
 
 use App\Filament\Resources\LeadResource;
+use AymanAlhattami\FilamentContextMenu\Actions\GoBackAction;
+use AymanAlhattami\FilamentContextMenu\Actions\GoForwardAction;
+use AymanAlhattami\FilamentContextMenu\Actions\RefreshAction;
+use AymanAlhattami\FilamentContextMenu\Traits\PageHasContextMenu;
 use Filament\Actions;
 use Filament\Resources\Pages\ListRecords;
 
 class ListLeads extends ListRecords
 {
     protected static string $resource = LeadResource::class;
+    use PageHasContextMenu;
 
+    public static function isContextMenuEnabled(): bool
+    {
+        return true;
+    }
+
+    //
+
+    public static function getContextMenuActions(): array
+    {
+        return [
+            RefreshAction::make(),
+            GoBackAction::make(),
+            GoForwardAction::make()
+        ];
+    }
     protected function getHeaderActions(): array
     {
         return [
